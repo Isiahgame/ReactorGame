4d2b9c179315fce83a42b1d38b19028dcd202d02
Joshua Irish-Donges <1005809@student.sae.edu.au>
2016-08-30 13:39:22 +1000

    Added UI and Buildplace function for the cubes on the first island

---
+++ Assets/Scripts/BuildPlace.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildPlace : MonoBehaviour {
+    // The block to be placed
+    public GameObject AnchorPoint;
+    public GameObject PrefabToSpawn;
+
+    private GameObject AttachedBuilding = null;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ReactToMouseClick()
+    {
+        if (AttachedBuilding != null)
+        {
+            GameObject.Destroy(AttachedBuilding);
+            AttachedBuilding = null;
+        }
+        else
+        {
+            // Spawn the new prefab
+            AttachedBuilding = GameObject.Instantiate(PrefabToSpawn);
+
+            //position the prefav at the anchor point
+            AttachedBuilding.transform.position = AnchorPoint.transform.position;
+            AttachedBuilding.transform.SetParent(AnchorPoint.transform);
+
+        }
+    }
+}
