497a30bd6716adbe628ba7bbc4e108a980e2f51d
Joshua Irish-Donges <1005809@student.sae.edu.au>
2016-08-31 13:41:17 +1000

    Added the updating of the variables the player must manage

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+	public static GameManager Current;
+
+	public float Money = 1;
+	public float MoneyGained = 0;
+	public float Research = 0;
+	public float ResearchGained = 0;
+	public float CurrentPower = 0;
+	public float PowerGained = 0;
+	public float MaxPower = 50;
+	public float StoragePercent = 0;
+
+	void Awake()
+	{
+		Current = this;
+	}
 
 	// Use this for initialization
 	void Start () {
+		
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		StoragePercent = (CurrentPower / MaxPower) * 100;
 	}
 }
--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -1,15 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UI_Controller : MonoBehaviour {
+	public EventSystem LevelEventSystem;
 
+	public static UI_Controller Current;
+
+	public Text MoneyGained;
+	public Text ResearchGained;
+	public Text PowerGained;
+	public Text MaxPower;
+	public Text StoragePercent;
+	public Slider StorageRepresentation;
+
+
+	void Awake()
+	{
+		Current = this;
+	}
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
+		MoneyGained.text = GameManager.Current.Money + " +" + GameManager.Current.MoneyGained;
+		ResearchGained.text = GameManager.Current.Research + " +" + GameManager.Current.ResearchGained;
+		PowerGained.text = GameManager.Current.CurrentPower + " +" + GameManager.Current.PowerGained;
+		MaxPower.text = GameManager.Current.MaxPower.ToString();
+		StoragePercent.text = GameManager.Current.StoragePercent + "%";
+		StorageRepresentation.value = GameManager.Current.StoragePercent;
+
+	// Has the user clicked the primary mouse button
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			// Is the current mouse location over the UI?
+			if (LevelEventSystem.IsPointerOverGameObject ()) {
+				
+			} else 
+			{
+				//Setup the ray based on the mosue location
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+				// Perform a raycast to determine what we have hit
+				RaycastHit HitResults;
+				if (Physics.Raycast (ray, out HitResults)) 
+				{
+					// Retrieve the gameobject that was hit
+					GameObject HitObject = HitResults.collider.gameObject;
+
+					BuildPlace buildplace = HitObject.GetComponent<BuildPlace> ();
+					if (buildplace != null) {
+						buildplace.ReactToMouseClick ();
+					} else {
+						// Attempt to find buildplace as parent
+						buildplace = HitObject.GetComponentInParent<BuildPlace> ();
+						if (buildplace != null) {
+							buildplace.ReactToMouseClick ();
+						}
+					}
+				}
+			}
+		}
+			
 	}
 }
