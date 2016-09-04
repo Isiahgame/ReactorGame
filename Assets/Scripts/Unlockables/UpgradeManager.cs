397f5d6073f94e5bd49bd2418cdfb3e65fcd0b39
Joshua Irish-Donges <1005809@student.sae.edu.au>
2016-09-04 14:06:34 +1000

    Have added the upgrade screen and implementing the code to mange the buildings levels

--- Assets/Scripts/Unlockables/UpgradeManager.cs
+++ Assets/Scripts/Unlockables/UpgradeManager.cs
@@ -1,7 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UpgradeManager : MonoBehaviour {
+
+	//building Lvl
+	public Text WindmillLvlText;
+	public Text WindmillLifeLVLText;
+	public Text OfficeLVlText;
+	public Text ResearchLvlText;
+	public Text BatteryLVLText;
+	public Text SolarHeatLVLText;
+	public Text SolarLifeLVLText;
+	public Text GenHeatLVLText;
+
+	//Building Cost
+	public Text WindmillCostText;
+	public Text WindmillLifeCostText;
+	public Text OfficeCostText;
+	public Text ResearchCostText;
+	public Text BatteryCostText;
+	public Text SolarHeatCostText;
+	public Text SolarLifeCostText;
+	public Text GenHeatCostText;
+
+	//Float Values
+	public float WindmillLvl = 1;
+	public float WindmillLifeLVL = 1;
+	public float OfficeLVl = 1;
+	public float ResearchLvl = 1;
+	public float BatteryLVL = 1;
+	public float SolarHeatLVL = 1;
+	public float SolarLifeLVL = 1;
+	public float GenHeatLVL = 1;
+
+	//Building Cost
+	public float WindmillCost;
+	public float WindmillLifeCost;
+	public float OfficeCost;
+	public float ResearchCost;
+	public float BatteryCost;
+	public float SolarHeatCost;
+	public float SolarLifeCost;
+	public float GenHeatCost;
+
 
 	// Use this for initialization
 	void Start () {
